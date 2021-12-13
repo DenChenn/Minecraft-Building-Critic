2fede4743c143b01d796d6e12a71c605dc43f426
SwarzChen <aaaa102234@gmail.com>
2021-12-13 20:29:59 +0800

    :tada: [main] Added my project

---
+++ src/main/java/me/yentingchen/aibuildingcritic/AIbuildingCritic.java
@@ -0,0 +1,16 @@
+package me.yentingchen.aibuildingcritic;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AIbuildingCritic extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello World!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
